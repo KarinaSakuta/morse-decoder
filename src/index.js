61f12ebd2de1285622eed5745701cc8ab58a792b
Karina <47184215+KarinaSakuta@users.noreply.github.com>
2019-10-19 23:21:54 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -37,8 +37,51 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+function getDigitalMorseCode(morseCode) {
+    var digitalCode = '0000000000'.split('');
+    var letterCode = morseCode.replace(/\./g, '10').replace(/-/g, '11');
+
+    for (var letterIndex = 0; letterIndex < digitalCode.length; letterIndex++) {
+        var matchingLetterCodeIndex = letterIndex - (digitalCode.length - letterCode.length);
+
+        digitalCode[letterIndex] = letterCode[matchingLetterCodeIndex] || '0';
+    }
+
+    return digitalCode.join('');
+}
+
+function getDigitalMorseTable() {
+    var morseCodes = Object.keys(MORSE_TABLE);
+    var digitalMorseTable = {};
+
+    for (var index = 0; index < morseCodes.length; index++) {
+        var morseCode = morseCodes[index];
+        var digitalCode = getDigitalMorseCode(morseCode);
+
+        digitalMorseTable[digitalCode] = MORSE_TABLE[morseCode];
+    }
+
+    digitalMorseTable['**********'] = ' ';
+
+    return digitalMorseTable;
+}
+
 function decode(expr) {
-    // write your solution here
+    var digitalMorseTable = getDigitalMorseTable();
+
+    var tmpString = '';
+    var result = '';
+
+    for (var index = 0; index < expr.length; index++) {
+        tmpString += expr[index];
+
+        if (digitalMorseTable[tmpString]) {
+            result += digitalMorseTable[tmpString];
+            tmpString = '';
+        }
+    }
+
+    return result;
 }
 
 module.exports = {
